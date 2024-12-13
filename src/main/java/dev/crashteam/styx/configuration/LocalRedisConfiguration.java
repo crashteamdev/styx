71fd1667774ab8b6c8ebd29372213d42c42c99a1
SageFreke <lenigrast@gmail.com>
2024-12-13 16:07:06 +0300

    add redis local config

---
+++ src/main/java/dev/crashteam/styx/configuration/LocalRedisConfiguration.java
@@ -0,0 +1,31 @@
+package dev.crashteam.styx.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@ConditionalOnProperty(value = "app.redis.local", havingValue = "true")
+public class LocalRedisConfiguration {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName(redisHost);
+        redisStandaloneConfig.setPort(redisPort);
+        redisStandaloneConfig.setPassword(redisPassword);
+        return new LettuceConnectionFactory(redisStandaloneConfig);
+    }
+}
